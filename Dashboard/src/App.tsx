f6ae53c29abdde982ce3ac62928a5d2cdca1a840
Rishil Patel <168255504+RishilPatel05@users.noreply.github.com>
2025-12-06 00:53:08 -0800

    Create App component for FrostDashboard

---
+++ Dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+
+import React from 'react';
+import FrostDashboard from './components/FrostDashboard';
+import './index.css'; // Import your Tailwind CSS file
+
+function App() {
+  return (
+    <FrostDashboard />
+  );
+}
+
+export default App;
